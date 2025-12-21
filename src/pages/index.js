c1dd72390bc13636c8386781c2a64a98a768a318
A7MED970 <A.Shaban2388@nu.edu.eg>
2025-12-21 20:15:06 +0200

    link admin setting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+export { default as AdminSettingsPage } from "./AdminSettingsPage";
 export { default as LoginPage } from "./LoginPage";
 export { default as RegisterPage } from "./RegisterPage";
 export { default as DashboardPage } from "./DashboardPage";
